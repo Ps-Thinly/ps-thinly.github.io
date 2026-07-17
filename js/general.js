3fcb2ec2b00451795fb4b2d1b91f9bf17b323047
Ps-Thinly <joonas.siermala@proton.me>
2026-07-17 21:44:57 +0300

    added seperated remove buttons for both strange and golden rocks

--- js/general.js
+++ js/general.js
@@ -1,7 +1,30 @@
+//poistaa koko storagen tyhjäks
 const button = document.querySelector("#reset-button");
 
 button.addEventListener("click", () => {
-    console.log("Nappia klikattiin!");
     localStorage.clear();
+    location.reload();
+});
+//tyhjentää vain strange rocksit
+const removeStranges = document.querySelector("#remove-stranges");
+removeStranges.addEventListener("click", () => {
+
+    for (let i = 0; i <= 14; i++) {
+    localStorage.removeItem("strangerock1-" + i);
+    localStorage.removeItem("strangerock2-" + i);
+}
+    localStorage.removeItem("lastStrangeRock");
+    location.reload();
+
+});
+//tyhjentää vain golden rocksit
+const removeGoldens = document.querySelector("#remove-goldens");
+removeGoldens.addEventListener("click", () => {
+
+    for (let i = 0; i <= 14; i++) {
+    localStorage.removeItem("goldenrock1-" + i);
+    localStorage.removeItem("goldenrock2-" + i);
+}
+    localStorage.removeItem("lastGoldenRock");
     location.reload();
 });
